dbb4e1c57636833113188bef0fd49dc28182c3a0
weichuwang <weichuwang2015@gmail.com>
2022-11-06 20:27:24 -0600

    arrayintlist

--- ArrayList/ArrayIntList.java
+++ ArrayList/ArrayIntList.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class ArrayIntList {
+public class ArrayIntList {
+    private int[] elementData;
+    private int size;
+
+    public ArrayIntList(int size){
+        int[] elementData = new int[size];
+        size = size;
+    }
+
+    public String toString() {
+        String result = "[";
+        if (size != 0){
+            result += elementData[0];
+            for (int i = 1; i <size; i++) {
+                result += ", "+ elementData[i];
+            }
+        }
+        result += "]";
+        return result;
+    }
+
+    public void add(int value){
+        elementData[size] = value;
+        size++;
+    }
+
+
+
+
 }
